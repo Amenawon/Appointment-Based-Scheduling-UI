2118bb51f2b89c4cf3562b0cf30888de37064ed2
ValeriyaMix <lerka-mishka@yandex.ru>
2025-04-28 22:27:34 +0100

    Initial commit of the Authentication page.

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+
+function AuthPage() {
+    return (
+        <div>
+            <h1>Login page</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Submit</button>
+        </div>
+    );
+}
+
+export default AuthPage;
