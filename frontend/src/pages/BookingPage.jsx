59a63e3bd2601655b07b0178eff244691e9e3d7b
ValeriyaMix <lerka-mishka@yandex.ru>
2025-04-29 23:30:34 +0100

    Add a package for date-picker. Add date picker to the booking page.

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -1,9 +1,14 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './BookingPage.css';
+import React, { useState } from 'react';
 
 function BookingPage() {
+    const [selectedDate, setSelectedDate] = useState(null);
     return (
-        <div className="booking-container">
+        <div>
             <h1>Booking page</h1>
+        <div className="booking-container">
             <div className="column left-column">
                 <h2>Choose attendees</h2>
                 <input type="text" placeholder="Enter attendees here" className="attendees-textbox"/>
@@ -15,11 +20,17 @@ function BookingPage() {
                 <input type="text" placeholder="Enter location here" className="appointment-location-textbox"/>
             </div>
             <div className="column middle-column">
-                <h1>Choose appointment date</h1>
+                <h2>Choose appointment date</h2>
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={(date) => setSelectedDate(date)}
+                    placeholderText="Select appointment date"
+                />
             </div>
             <div className="column right-column">
-                <h1>Available time slots</h1>
+                <h2>Available time slots</h2>
             </div>
+        </div>
         </div>
     );
 }
