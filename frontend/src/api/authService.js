2060333b00d05fb674e97001dacc1aaa571ea059
ValeriyaMix <lerka-mishka@yandex.ru>
2025-06-22 12:31:32 +0100

    Remove unnecessary comments.

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -3,21 +3,22 @@ import { jwtDecode } from 'jwt-decode';
 
 export async function login(email, password) {
         try {
+
             const response = await apiClient.post('/api/User/login', { email, password });
-                    // If the server returns a token?
-        const { token } = response.data;
+        
+            const { token } = response.data;
 
-        console.log('Response data:', response.data);
+            console.log('Response data:', response.data);
 
-        console.log('Token:', token.result);
+            console.log('Token:', token.result);
 
-        const decoded = jwtDecode(token.result);
-        const emailClaim = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
-        const userEmail = decoded[emailClaim];
-        console.log('Email:', userEmail);
+            const decoded = jwtDecode(token.result);
+            const emailClaim = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+            const userEmail = decoded[emailClaim];
+            console.log('Email:', userEmail);
 
-        localStorage.setItem('token', token);
-        localStorage.setItem('userEmail', userEmail);
+            localStorage.setItem('token', token);
+            localStorage.setItem('userEmail', userEmail);
 
         return { 
             success: true,
